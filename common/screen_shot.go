cca67d3ca40c3fa5d5fb748a8aec764505403ab6
Nguyễn Đắc Toàn <nguyendactoankma@gmail.com>
2022-01-28 14:16:48 +0700

    Update screen_shot.go

--- common/screen_shot.go
+++ common/screen_shot.go
@@ -2,7 +2,7 @@ package common
 
 import "github.com/chromedp/chromedp"
 
-// ElementScreenshot takes a screenshot of a specific element.
+// ElementScreenShot takes a screenshot of a specific element.
 func ElementScreenShot(urlstr, sel string, res *[]byte) chromedp.Tasks {
 	return chromedp.Tasks{
 		chromedp.Navigate(urlstr),
